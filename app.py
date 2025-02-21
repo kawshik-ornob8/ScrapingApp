f2bba91588341e9824b20aba050588667e7cdd50
Kawshik Ahmed Ornob <97808635+kawshik-ornob8@users.noreply.github.com>
2025-02-21 06:09:57 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
+
 from flask import Flask
 from routes import register_routes
-from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
-@app.template_filter('datetimefilter')
-def datetime_filter(value, format='%Y'):
-    return datetime.now().strftime(format)
-
 register_routes(app)
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
